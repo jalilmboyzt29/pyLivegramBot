6751a524dab9d949377f6fdeb010a0d18315fa45
jalilmboyzt29 <92901357+jalilmboyzt29@users.noreply.github.com>
2021-11-14 11:46:48 +0900

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@
 #Written by @coder2020 - telegram account.
 
 blocked = "bot was blocked by the user"
-start = "Hello! This is your start message!"
+start = "Hello! Terimakasih sudah menghubungi kami melalui Bot ini, apa yang dapat kami bantu? 🙏!"
 ban = "you were banned by the admin!"
 unban = "you were unbanned by the admin."
 text_message = "Message that would be send if somebody writes any text"
 banned = "you are blocked"
-main_id = -1111 #id without quotes #can be group id REMEMBER!: SUPERGROUP ID BEGINS WITH MINUS
-TOKEN = "######:############"
+main_id = -1001324718011 #id without quotes #can be group id REMEMBER!: SUPERGROUP ID BEGINS WITH MINUS
+TOKEN = "2115557828:AAEV64DUFLP3arr7xtfwjoE8lbBFYLJMIWc"
 notallowed = "Do not forward me a message."
